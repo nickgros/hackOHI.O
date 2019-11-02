429e26cc724452753614aa0bf616f3e566b4b49a
Nick Grosenbacher <nickgrosenbacher@gmail.com>
2019-11-02 11:51:23 -0400

    add OSM

--- spatial_data.R
+++ spatial_data.R
@@ -3,6 +3,7 @@ library(raster)
 library(sp)
 library(rgdal)
 library(tidyverse)
+library(leaflet)
 
 # Read in data and create a coordinate df ---------------------------------
 configHeaders <- c("BuildingID", "MeterID", "Description", "Units", "Resource", "BuildingName", "GrossSquareFeet", "BuildDate", "Latitude", "Longitude", "Campus", "Organization", "LocationType", "SteamSourceID", "ChilledWaterSourceID", "HotWaterSourceID", "Time")
@@ -14,14 +15,8 @@ building_dat$HotWaterSourceID <- as.factor(building_dat$HotWaterSourceID)
 building_dat$ChilledWaterSourceID <- as.factor(building_dat$ChilledWaterSourceID)
 building_dat$SteamSourceID <- as.factor(building_dat$SteamSourceID)
 
-building_dat <- building_dat[!is.na(building_dat$Longitude),]
+building_dat <- building_dat[!is.na(building_dat$Longitude) & building_dat$Longitude != 0,]
 
-
-coords <- data.frame(X  = building_dat$Longitude,
-                     Y = building_dat$Latitude)
-coords <- coords[coords$X != 0,]
-
-cpoints <- SpatialPoints(coords) #Spatial points object with no other definitions than just coordinates
-
-
-
+m <- leaflet() %>%
+  addTiles() %>%  # Add default OpenStreetMap map tiles
+  addMarkers(lng=building_dat$Longitude, lat=building_dat$Latitude, popup=building_dat$BuildingName)
